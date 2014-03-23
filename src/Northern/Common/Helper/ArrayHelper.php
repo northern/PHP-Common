3a5cd6a1dbf6fa63355d410c8bbe1878605deb0f
Luke Schreur <luke.schreur@gmail.com>
2014-03-24 10:54:39 +1100

    Added merge method to ArrayHelper.

--- src/Northern/Common/Helper/ArrayHelper.php
+++ src/Northern/Common/Helper/ArrayHelper.php
@@ -381,4 +381,32 @@ abstract class ArrayHelper {
 		return $results;
 	}
 	
+	/**
+	 * Recursive merges two arrays together.
+	 * 
+	 * Source: http://au1.php.net/manual/en/function.array-merge-recursive.php#92195
+	 * 
+	 * @param array $array1
+	 * @param array $array2
+	 * @return array
+	 */
+	static public function merge( array $array1, array $array2 )
+	{
+		$merged = $array1;
+		
+		foreach( $array2 as $key => &$value )
+		{
+			if( is_array ( $value ) && isset ( $merged [$key] ) && is_array ( $merged [$key] ) )
+			{
+				$merged [$key] = static::merge( $merged [$key], $value );
+			}
+			else
+			{
+				$merged [$key] = $value;
+			}
+		}
+		
+		return $merged;
+	}
+
 }
