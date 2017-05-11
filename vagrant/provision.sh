f9158ddb2fc830bd64a6d82355a581b58b92165f
Luke Schreur <luke.schreur@gmail.com>
2017-05-11 12:28:53 +1000

    Added Vagrant profile and provisioning script.

---
+++ vagrant/provision.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Add swap space.
+sh /vagrant/vagrant/swap.sh
+
+# Add PHP repos.
+add-apt-repository ppa:ondrej/php
+
+# Install prep.
+apt-get update
+#apt-get -y upgrade
+
+# Install basic necessities.
+apt-get -y install ntp git htop tree zip unzip python-simplejson
+systemctl restart ntp.service
+
+# Install php.
+PHP_VERSION="5.6"
+
+apt-get -y install \
+  php${PHP_VERSION} \
+  php${PHP_VERSION}-cli \
+  php${PHP_VERSION}-curl \
+  php${PHP_VERSION}-mcrypt \
+  php${PHP_VERSION}-intl \
+  php${PHP_VERSION}-json \
+  php${PHP_VERSION}-xml \
+  php${PHP_VERSION}-mbstring
+  php-xdebug
+
+sed -i "s/;date timezone =/date timezone = UTC/" /etc/php/${PHP_VERSION}/apache2/php.ini
+sed -i "s/;date timezone =/date timezone = UTC/" /etc/php/${PHP_VERSION}/cli/php.ini
+
+sed -i "s/;realpath_cache_size = 16k/realpath_cache_size = 4096k/" /etc/php/${PHP_VERSION}/apache2/php.ini
+sed -i "s/;realpath_cache_size = 16k/realpath_cache_size = 4096k/" /etc/php/${PHP_VERSION}/cli/php.ini
+
+sed -i "s/;realpath_cache_ttl = 120/realpath_cache_size = 7200/" /etc/php/${PHP_VERSION}/apache2/php.ini
+sed -i "s/;realpath_cache_ttl = 120/realpath_cache_size = 7200/" /etc/php/${PHP_VERSION}/cli/php.ini
